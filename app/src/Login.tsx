5e115608fab8ae3ad7a48c2d6488dcbf7bf2c0ae
mjBerhe <mjberhe123@hotmail.com>
2023-11-02 15:50:56 -0400

    initial config

---
+++ app/src/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Login = () => {
+  return <div className="border text-blue-500">hello login pageeee</div>
+}
+
+export default Login
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        host: '0.0.0.0',
+  plugins: [react()],
+  server: {
+    port: 3000,
+    // host: "0.0.0.0",
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login/index.html"),
+      },
     },
+  },
 })
